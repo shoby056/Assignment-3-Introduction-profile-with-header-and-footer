01c683aabab8e2f43128a64ed54deadce55b18c7
Muhammad Shoaib <136969366+shoby056@users.noreply.github.com>
2024-09-30 15:37:32 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/page"
+import Footer from "../components/footer"
+export default function Jobs(){
+  return(
+    <div>
+   <Header/>
+   <h1>Its the page of Jobs</h1>
+   <Footer/>
+   
+    </div>
+  )
+}
---
+++ section.tsx
@@ -0,0 +1,11 @@
+export default function Section(){
+    return(
+        <div className="container">
+  
+  
+          <div className="left-container">Hi
+            ,<br />Iam Muhammad Shoaib Shaikh and Iam Currently  Web developer at Giaic and Junior software Engineer  </div>
+          <div className="right-container"><img src="https://avatars.githubusercontent.com/u/136969366?v=4" alt="image" className="img" /></div>
+        </div>
+    )
+}
